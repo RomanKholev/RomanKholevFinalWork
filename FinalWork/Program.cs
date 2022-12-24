1d2e415f49437a80b506d3bc1df9cff88e41ff52
Roman Kholev <RKholev@gmail.com>
2022-12-24 13:46:51 +0300

    Add SerrchAndCount in program

--- FinalWork/Program.cs
+++ FinalWork/Program.cs
@@ -19,3 +19,38 @@ void PrintArrey(string [] a)
     for(int i=0;i<a.Length;i++)
         System.Console.WriteLine($"{a[i],5}");
 }
+
+void PrintArrey(string [] a)
+{
+    for(int i=0;i<a.Length;i++)
+        System.Console.WriteLine($"{a[i],5}");
+}
+
+string [] SerrchAndCount (string [] a)
+{
+    string [] finalarrey;
+    int j=0;
+        for (int i = 0; i <a.Length; i++)
+    {
+        if (a[i].Length<=3) j++;
+    }
+if (j==0)
+    {
+        finalarrey=new string[1];
+        finalarrey[0]="Строк длинной 3 и мене символов не найдено";
+    } 
+else
+    {
+        finalarrey=new string[j]; 
+        int count=0;
+        for (int i = 0; i <a.Length; i++)
+        {
+         if (a[i].Length<=3) 
+         {
+          finalarrey[count]=a[i];
+          count++;
+         }  
+        }
+    }
+    return finalarrey;
+}
